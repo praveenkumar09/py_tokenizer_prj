635b93ee48cc25f019edb56f27f0b1655ed4bad7
praveenkumar09 <tpkprav@gmail.com>
2025-07-20 12:53:47 +0800

    Added token to vocab

--- tokenizer.py
+++ tokenizer.py
@@ -5,6 +5,20 @@ from nltk.tokenize import word_tokenize
 import spacy
 from transformers import BertTokenizer
 from transformers import XLNetTokenizer
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+
+dataset = [
+    (1,"Introduction to NLP"),
+    (2,"Basics of PyTorch"),
+    (1,"NLP Techniques for Text Classification"),
+    (3,"Named Entity Recognition with PyTorch"),
+    (3,"Sentiment Analysis using PyTorch"),
+    (3,"Machine Translation with PyTorch"),
+    (1," NLP Named Entity,Sentiment Analysis,Machine Translation "),
+    (1," Machine Translation with NLP "),
+    (1," Named Entity vs Sentiment Analysis  NLP ")]
+
 
 def warn(*args,**kwargs):
     pass
@@ -36,9 +50,25 @@ def subword_wordpiece_based_tokenizer_xlnet():
     tokens = tokenizer.tokenize(text)
     return tokens
 
+
+def yield_tokens(datasetList):
+    tokenizer = get_tokenizer("basic_english")
+    for _,data in datasetList:
+     yield tokenizer(data)
+
 if __name__ == '__main__':
     print("NLKT tokens:",word_based_tokenizer_nltk())
     print("Spacy tokens:",word_based_tokenizer_spacy())
     print("Subword Bert Wordpiece tokens:",subword_wordpiece_based_tokenizer_bert())
     print("Subword XLNet Wordpiece tokens:",subword_wordpiece_based_tokenizer_xlnet())
+    tokenlist = yield_tokens(dataset)
+    vocab = build_vocab_from_iterator(yield_tokens(dataset),specials=["<unk>"])
+    vocab.set_default_index(vocab["<unk>"])
+    tokenIndices = []
+    for data in dataset:
+        tokenWord = next(tokenlist)
+        token_indices = [vocab[token] for token in tokenWord]
+        print("token_indices:",token_indices)
+        print("token_word:",tokenWord)
+
 
